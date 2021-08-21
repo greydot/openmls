2d41dab49de0c29a4dbf08f766fbd045af0ca165
Lana Black <lana@illuminati.industries>
2021-08-21 23:53:53 +0000

    Introduce shell.nix for better shinier workflow.

---
+++ shell.nix
@@ -0,0 +1,27 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let libclang = pkgs.llvmPackages_12.libclang;
+    stdenv = pkgs.clang12Stdenv;
+    lib = pkgs.lib;
+in
+pkgs.mkShell {
+  name = "openmls-shell";
+
+  buildInputs = with pkgs; [ libclang openssl ];
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+  LIBCLANG_PATH = "${libclang.lib}/lib";
+  shellHook = ''
+    # From: https://github.com/NixOS/nixpkgs/blob/1fab95f5190d087e66a3502481e34e15d62090aa/pkgs/applications/networking/browsers/firefox/common.nix#L247-L253
+    # Set C flags for Rust's bindgen program. Unlike ordinary C
+    # compilation, bindgen does not invoke $CC directly. Instead it
+    # uses LLVM's libclang. To make sure all necessary flags are
+    # included we need to look in a few places.
+    export BINDGEN_EXTRA_CLANG_ARGS="$(< ${stdenv.cc}/nix-support/libc-crt1-cflags) \
+      $(< ${stdenv.cc}/nix-support/libc-cflags) \
+      $(< ${stdenv.cc}/nix-support/cc-cflags) \
+      $(< ${stdenv.cc}/nix-support/libcxx-cxxflags) \
+      ${lib.optionalString stdenv.cc.isClang "-idirafter ${stdenv.cc.cc}/lib/clang/${lib.getVersion stdenv.cc.cc}/include"} \
+      ${lib.optionalString stdenv.cc.isGNU "-isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc} -isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${lib.getVersion stdenv.cc.cc}/include"} \
+    "
+  '';
+}
